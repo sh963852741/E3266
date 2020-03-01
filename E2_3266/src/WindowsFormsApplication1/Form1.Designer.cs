697f2a9e64f421f936e43e36c6f83e9004d317e8
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-03-01 13:00:09 +0800

    Add files via upload

---
+++ E2_3266/src/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.inputBox.Location = new System.Drawing.Point(12, 364);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(768, 148);
+            this.inputBox.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 544);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(792, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(705, 518);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "发送";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ReadOnly = true;
+            this.richTextBox.Size = new System.Drawing.Size(768, 346);
+            this.richTextBox.TabIndex = 4;
+            this.richTextBox.Text = "";
+            // 
+            // portBox
+            // 
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(47, 518);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(121, 20);
+            this.portBox.TabIndex = 5;
+            this.portBox.SelectionChangeCommitted += new System.EventHandler(this.portBox_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 523);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "端口";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 566);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "RS-232 通信程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ E2_3266/src/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort mySerialPort;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void OnDataReceived(Object sender, SerialDataReceivedEventArgs e)
+        {
+            string message = "[RECV " + DateTime.Now.ToString() + "] " + ((SerialPort)sender).ReadLine() + "\n";
+            richTextBox.SelectionColor = Color.Blue;
+            richTextBox.AppendText(message);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            mySerialPort = new SerialPort();
+            string[] portNameArr = SerialPort.GetPortNames();
+            if (portNameArr.Length == 0)
+            {
+                portBox.Enabled = false;
+                sendButton.Enabled = false;
+            }
+            else
+            {
+                portBox.Items.AddRange(portNameArr);
+                portBox.Text = portNameArr[0];
+                mySerialPort.PortName = portNameArr[0];
+                mySerialPort.Open();
+            }    
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(OnDataReceived);
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            string message = "[SENT " + DateTime.Now.ToString() + "] " + inputBox.Text + "\n";
+            this.mySerialPort.Write(message);
+            richTextBox.SelectionColor = Color.Green;
+            richTextBox.AppendText(message);
+            inputBox.Text = string.Empty;
+        }
+
+        private void portBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            mySerialPort.Close();
+            mySerialPort.PortName = ((ComboBox)sender).Text;
+            try
+            {
+                mySerialPort.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString() + " 类型的异常",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
